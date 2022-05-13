d2301800f7db3c0b2fcff935846c55619891c196
greenT-Hee <heehee970322@gmail.com>
2022-05-13 17:51:00 +0900

    DOM 드롭다운

---
+++ DOM_드롭다운_실행/dropDown.js
@@ -0,0 +1,34 @@
+const contSelect = document.querySelector('.cont-select');
+const btnSelect = document.querySelector('.btn-select');
+const listMember = document.querySelector('.list-member');
+const option = document.querySelectorAll('.opt-member');
+
+// 토글버튼이 클릭 --> 메뉴 보인다 
+// list-member를 show
+
+btnSelect.addEventListener('click', function(){
+    listMember.classList.toggle('show');
+    // btnSelect.classList.add('on');
+    btnSelect.classList.toggle('on');
+})
+
+btnSelect.addEventListener('blur', function(){
+    listMember.classList.remove('show');
+    btnSelect.classList.remove('on');
+})
+
+option.forEach(function(item){
+    item.addEventListener('mousedown',function(e){
+        const value = e.currentTarget.textContent;
+        btnSelect.textContent = value;
+        btnSelect.classList.add('selected-style')
+        // listMember.classList.remove('show')
+        // btnSelect.classList.remove('on');
+    })
+})
+
+
+
+
+//마우스다운 이벤트는 블러보다 먼저 실행되는 이벤트래요!
+//https://stackoverflow.com/questions/39439115/how-to-execute-click-function-before-the-blur-function
